bb22fb6452e4f8bc39711206d33b8849c5f1c29c
Mathilda Morea <mathildamorea@gmail.com>
2026-06-03 23:46:20 +1000

    Updated Lab 5 Binary Search

--- lab5_functions_algorithms.py
+++ lab5_functions_algorithms.py
@@ -53,6 +53,27 @@ def bubble_sort(arr):
 
 print("Bubble Sort:", bubble_sort([5, 3, 8, 1, 2]))
 
+# Binary Search
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+sorted_numbers = [1, 2, 3, 4, 5]
+
+print("Binary Search:", binary_search(sorted_numbers, 4))
+
 # Fibonacci Sequence
 
 def fibonacci(n):
