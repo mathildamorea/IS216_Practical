f81754804118a5972a6d32e8415272b661f8a66e
Mathilda Morea <mathildamorea@gmail.com>
2026-06-04 01:01:33 +1000

    Completed Lab 8 Frameworks and Functional Programming

--- lab8_functional_programming.py
+++ lab8_functional_programming.py
@@ -1,4 +1,22 @@
-# Functional Programming Examples
+# Lab 8 - Framework & Functional Programming
+
+# ------------------------------------------------
+# 1. Hello World Framework Example (Django)
+#-------------------------------------------------
+
+# Example Django Hello World
+
+# from django.http import HttpResponse
+#
+# def hello(request):
+#       return HttpResponse("Hello, Django")
+
+print("Hello, Django")
+
+
+# ------------------------------------------------
+# 2. Functional Programming Examples
+# ------------------------------------------------
 
 nums = [1, 2, 3, 4, 5]
 
@@ -24,14 +42,30 @@ print("Reduce:", total)
 def multiply(a, b):
     return a * b
 
-print("Pure Function:", multiply(5, 3))
-
-# Higher-Order Function
-
 def add(a, b):
     return a + b
+
+print("Pure Function Multiply:", multiply(5, 3))
+print("Pure Function Add:", add(5, 3))
+
+# Higher-Order Function
 
 def calculate(function, x, y):
     return function(x, y)
 
 print("Higher-Order Function:", calculate(add, 5, 3))
+
+# Imperative Style
+
+squares = []
+
+for num in nums:
+    squares.append(num * num)
+
+print("Imperative:", squares)
+
+# Functional Style
+
+functional_squares = list(map(lambda num: num * num, nums))
+
+print("Functional:", functional_squares)
